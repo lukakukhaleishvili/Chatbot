17c863ff59f12a2662e9efe81c2bb80d80b77a63
lukakukhaleishvili <127890018+lukakukhaleishvili@users.noreply.github.com>
2025-07-03 03:52:41 +0400

    Add files via upload

---
+++ ChatMessage.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+public class ChatMessage
+{
+    [Key]
+    public int Id { get; set; }
+
+    [Required]
+    public string User { get; set; }
+
+    [Required]
+    public string Message { get; set; }
+
+    public DateTime Timestamp { get; set; } = DateTime.UtcNow;
+}
